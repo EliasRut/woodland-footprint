36d9f2ca1e71992a026dc0dd68c03ea0158248a1
Elias Rut <eli4srut@gmail.com>
2022-04-10 21:27:48 +0200

    Started adding a parallax effect.

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect } from "react";
+import styles from "./About.module.css";
 
-const About = () => <div>About</div>;
+const About = () => (
+  <main className={styles.wrapper}>
+    <section className={`${styles.section} ${styles.parallax} ${styles.bg1}`}>
+      <h1>Cute Kitten</h1>
+    </section>
+    <section className={`${styles.section} ${styles.static}`}>
+      <h1>Boring</h1>
+    </section>
+    <section className={`${styles.section} ${styles.parallax} ${styles.bg2}`}>
+      <h1>Fluffy Kitten</h1>
+    </section>
+  </main>
+);
 
 export default About;
